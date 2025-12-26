34b82d1fabed76b5197fcb74407891db60907be2
Sanjivanee Todakar <sanjivanee244@gmail.com>
2025-12-26 12:05:36 +0530

    Merge remote and local history

--- arithmetic.py
+++
@@ -1,9 +0,0 @@
-# arithmetic.py
-
-def add(a, b):
-    """Return addition of two numbers"""
-    return a + b
-
-def subtract(a, b):
-    """Return subtraction of two numbers"""
-    return a - b
--- geometry.py
+++
@@ -1,9 +0,0 @@
-# geometry.py
-
-def area(length, breadth):
-    """Return area of rectangle"""
-    return length * breadth
-
-def perimeter(length, breadth):
-    """Return perimeter of rectangle"""
-    return 2 * (length + breadth)
